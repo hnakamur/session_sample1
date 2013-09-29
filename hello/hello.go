e4707243483c9c6e4d1fcd299367db4a3fd4ab41
Hiroaki Nakamura <hnakamur@gmail.com>
2013-09-30 00:03:15 +0900

    Using the Datastore.

--- hello/hello.go
+++ hello/hello.go
@@ -1,23 +1,56 @@
 package hello
 
 import (
-	"fmt"
 	"html/template"
 	"net/http"
+	"time"
+
+	"appengine"
+	"appengine/datastore"
+	"appengine/user"
 )
+
+type Greeting struct {
+	Author  string
+	Content string
+	Date    time.Time
+}
 
 func init() {
 	http.HandleFunc("/", root)
 	http.HandleFunc("/sign", sign)
 }
 
-func root(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, guestbookForm)
+func guestbookKey(c appengine.Context) *datastore.Key {
+	return datastore.NewKey(c, "Guestbook", "default_guestbook", 0, nil)
 }
 
-const guestbookForm = `
+func root(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	q := datastore.NewQuery("Greeting").Ancestor(guestbookKey(c)).Order("-Date").Limit(10)
+	greetings := make([]Greeting, 0, 10)
+	if _, err := q.GetAll(c, &greetings); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := guestbookTemplate.Execute(w, greetings); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+var guestbookTemplate = template.Must(template.New("book").Parse(guestbookTemplateHTML))
+
+const guestbookTemplateHTML = `
 <html>
   <body>
+  {{range .}}
+    {{with .Author}}
+	<p><b>{{.}}</b> wrote:</p>
+	{{else}}
+	<p>An anonymous person wrote:</p>
+	{{end}}
+	<pre>{{.Content}}</pre>
+  {{end}}
     <form action="/sign" method="post">
 	  <div><textarea name="content" rows="3" cols="60"></textarea></div>
 	  <div><input type="submit" value="Sign Guestbook"></div>
@@ -27,19 +60,19 @@ const guestbookForm = `
 `
 
 func sign(w http.ResponseWriter, r *http.Request) {
-	err := signTemplate.Execute(w, r.FormValue("content"))
+	c := appengine.NewContext(r)
+	g := Greeting{
+		Content: r.FormValue("content"),
+		Date:    time.Now(),
+	}
+	if u := user.Current(c); u != nil {
+		g.Author = u.String()
+	}
+	key := datastore.NewIncompleteKey(c, "Greeting", guestbookKey(c))
+	_, err := datastore.Put(c, key, &g)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
+	http.Redirect(w, r, "/", http.StatusFound)
 }
-
-var signTemplate = template.Must(template.New("sign").Parse(signTemplateHTML))
-
-const signTemplateHTML = `
-<html>
-  <body>
-    <p>You wrote:</p>
-	<pre>{{.}}</pre>
-  </body>
-</html>
-`
